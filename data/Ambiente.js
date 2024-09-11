3dcde24aebf2bff016fc8a81053c0c919beb4a4a
ThiagoMdO <thiago_catureba@hotmail.com>
2024-09-11 13:15:16 -0300

    fix: correção banco de dados mokado, retornado para valores primarios

--- data/Ambiente.js
+++ data/Ambiente.js
@@ -1,7 +1,7 @@
 const Ambiente = Object.freeze({
     area1:  { recinto: 1, tamanhoTotal: 10, bioma:["savana"], habitantes: ["macaco,3"] },
-    area2:  { recinto: 2, tamanhoTotal: 10, bioma:["savana"], habitantes: ["hipopotamo,1"] },
-    area3:  { recinto: 3, tamanhoTotal: 10, bioma:["savana", "rio"], habitantes: ["hipopotamo,1"] },
+    area2:  { recinto: 2, tamanhoTotal: 5, bioma:["savana"], habitantes: [] },
+    area3:  { recinto: 3, tamanhoTotal: 7, bioma:["savana", "rio"], habitantes: ["gazela,1"] },
     area4:  { recinto: 4, tamanhoTotal: 8, bioma:["rio"], habitantes: [] },
     area5:  { recinto: 5, tamanhoTotal: 9, bioma:["savana"], habitantes: ["leão,1"] }
 });
